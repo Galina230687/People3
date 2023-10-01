6c9974c4c0defa729745a09a745fced41f5763d3
Galina <serena_05@mail.ru>
2023-10-01 16:34:34 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        Person mom = new PersonBuilder()
+                .setName("Анна")
+                .setSurname("Вольф")
+                .setAge(35)
+                .setAddress("Сидней")
+                .build();
+        Person son = mom.newChildBuilder()
+                .setName("Антошка")
+                .setAge(1)
+                .build();
+        System.out.println("У " + mom + " есть сын, " + son);
+
+        try {
+            // Не хватает обязательных полей
+            new PersonBuilder().setSurname(null).build();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            // Возраст недопустимый
+            new PersonBuilder().setAge(-100).build();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+}
